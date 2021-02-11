7e5a9a118f69e93d2e7f83372d50b413da8784aa
Joris <jorisnijkamp@gmail.com>
2021-02-11 16:01:07 +0100

    Working stop server. But maybe not so nice way

--- 03_ChatServerWPF/MainWindow.xaml.cs
+++ 03_ChatServerWPF/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,11 +14,10 @@ namespace _03_ChatServerWPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        // Stap 3:
         TcpClient tcpClient;
+        private TcpListener tcpListener;
         NetworkStream networkStream;
         private Boolean serverRunning;
-
         private List<TcpClient> clientConnectionList = new List<TcpClient>();
 
         public MainWindow()
@@ -27,24 +27,23 @@ namespace _03_ChatServerWPF
 
         private void AddMessage(string message)
         {
-            this.Dispatcher.Invoke(() => listChats.Items.Add(message));
+            // this.Dispatcher.Invoke(() => listChats.Items.Add(message));
+            listChats.Items.Add(message);
+            // listChats.SelectedIndex = listChats.Items.Count - 1;
         }
-        
+
         private async void startServerButton_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                if (!serverRunning)
-                {
-                    await CreateServerAsync(
-                        serverPort.Text,
-                        serverBufferSize.Text
-                    );
-                }
+                await CreateServerAsync(
+                    serverPort.Text,
+                    serverBufferSize.Text
+                );
             }
             catch
             {
-                MessageBox.Show("Server is not available");
+                MessageBox.Show("SERVER CANNOT BE STARTED");
             }
         }
 
@@ -52,7 +51,9 @@ namespace _03_ChatServerWPF
         {
             try
             {
+                AddMessage("Server is closing");
                 serverRunning = false;
+                tcpListener.Stop();
                 serverName.IsEnabled = true;
                 serverPort.IsEnabled = true;
                 serverBufferSize.IsEnabled = true;
@@ -62,11 +63,10 @@ namespace _03_ChatServerWPF
             }
             catch
             {
-                MessageBox.Show("Server is not available");
+                MessageBox.Show("SERVER CANT BE STOPPED");
             }
         }
 
-        
         private async Task CreateServerAsync(string port, string buffer)
         {
             serverRunning = true;
@@ -77,50 +77,54 @@ namespace _03_ChatServerWPF
             btnStart.Visibility = Visibility.Hidden;
             btnSend.IsEnabled = true;
 
-            TcpListener tcpListener = new TcpListener(IPAddress.Any, Int32.Parse(port));
+            tcpListener = new TcpListener(IPAddress.Any, Int32.Parse(port));
             tcpListener.Start();
-            
+
             AddMessage($"Listening for clients on port: {port}");
 
-            while (serverRunning)
+            Task.Run(async () =>
             {
-                TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
-                clientConnectionList.Add(tcpClient);
-                // await Task.Run(() => ReceiveData(tcpClient, Int32.Parse(buffer)));
-            }
-            tcpListener.Stop();
+                while (serverRunning)
+                {
+                    var tcpClient = await tcpListener.AcceptTcpClientAsync();
+
+                    clientConnectionList.Add(tcpClient);
+
+                    // await Task.Run(() => ReceiveData(tcpClient, Int32.Parse(buffer)));
+                }
+            });
         }
-        
-        private void ReceiveData(TcpClient tcpClient, int buffersize)
-        {
-            string message = "";
-            byte[] buffer = new byte[buffersize];
 
-            networkStream = tcpClient.GetStream();
-            
-            AddMessage("Connected!");
-            
-            while (true)
-            {
-                int readBytes = networkStream.Read(buffer, 0, buffersize);
-                message = Encoding.ASCII.GetString(buffer, 0, readBytes);
-
-                if (message == "bye")
-                    break;
-
-                AddMessage(message);
-            }
-
-            // Verstuur een reactie naar de client (afsluitend bericht)
-            buffer = Encoding.ASCII.GetBytes("bye");
-            networkStream.Write(buffer, 0, buffer.Length);
-
-            // cleanup:
-            networkStream.Close();
-            tcpClient.Close();
-
-            AddMessage("Connection closed");
-        }
+        // private void ReceiveData(TcpClient tcpClient, int bufferSize)
+        // {
+        //     string message = "";
+        //     byte[] buffer = new byte[buffersize];
+        //
+        //     networkStream = tcpClient.GetStream();
+        //     
+        //     AddMessage("Connected!");
+        //     
+        //     while (networkStream.CanRead)
+        //     {
+        //         int readBytes = networkStream.Read(buffer, 0, buffersize);
+        //         message = Encoding.ASCII.GetString(buffer, 0, readBytes);
+        //
+        //         if (message == "bye")
+        //             break;
+        //
+        //         AddMessage(message);
+        //     }
+        //
+        //     // Verstuur een reactie naar de client (afsluitend bericht)
+        //     buffer = Encoding.ASCII.GetBytes("bye");
+        //     networkStream.Write(buffer, 0, buffer.Length);
+        //
+        //     // cleanup:
+        //     networkStream.Close();
+        //     tcpClient.Close();
+        //
+        //     AddMessage("Connection closed");
+        // }
 
 
         private async void btnSend_Click(object sender, RoutedEventArgs e)
